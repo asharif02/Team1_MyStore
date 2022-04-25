073f748ba9d26180d298297fccb55785909b40dc
asharif02 <asharif02@gmail.com>
2022-04-24 23:01:49 -0400

    Team 1 MyStore Framework for Assignment 2

---
+++ src/test/java/test_app/smoke/transactions/ShoppingCart.java
@@ -0,0 +1,34 @@
+package test_app.smoke.transactions;
+
+import app.pom.Homepage;
+import app.pom.SummerDresses;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import test_base.TestBasePage;
+
+public class ShoppingCart extends TestBasePage {
+
+    // region Sami's Example Test Case
+    @Test
+    public void testAddItemToCart() {
+        Homepage homepage = new Homepage();
+        homepage.hoverOverWomenButton();
+        SummerDresses summerDresses = homepage.clickWomenHoverMenuDressesSummerDressesButton();
+
+        summerDresses.setUpperPriceRange(20.00);
+        summerDresses.hoverOverFirstItem();
+        summerDresses.clickPrintedChiffonDressQuickViewButton();
+
+        int desiredQuantity = 3;
+        String size = "M";
+        summerDresses.addItemToCart(desiredQuantity, size);
+
+        String expectedMessage = "Product successfully added to your shopping cart";
+
+        Assert.assertEquals(summerDresses.getShoppingCartModalSuccessOrFailMessage(), expectedMessage);
+        Assert.assertEquals(summerDresses.getShoppingCartModalQuantityCount(), desiredQuantity);
+
+    }
+    // endregion
+
+}
