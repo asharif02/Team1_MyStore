12163cf55369d7185d70b20328cc6e56425b4500
asharif02 <asharif02@gmail.com>
2022-04-26 10:54:11 -0400

    pushing 2 test cases

--- src/main/java/app/pom/Homepage.java
+++ src/main/java/app/pom/Homepage.java
@@ -1,14 +1,46 @@
 package app.pom;
 
 import app.shared.SystemBar;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class Homepage extends SystemBar {
+
+    @FindBy(id = "search_query_top")
+    public WebElement searchBoxInputField;
+
+    @FindBy(name = "submit_search")
+    public WebElement searchSubmitButton;
+
+    @FindBy(css = ".lighter")
+    public WebElement actualSearchTerm;
+
+    @FindBy(css = ".alert.alert-warning")
+    public WebElement noResultsFoundAlertWarning;
 
 
 
     public Homepage() {
         PageFactory.initElements(driver, this);
+    }
+
+    public Homepage searchForClothingItem(String clothingItem) {
+
+        inputSearchTerm(clothingItem);
+        clickSearchSubmitButton();
+
+        return new Homepage();
+
+    }
+
+    public String inputSearchTerm(String clothingItem) {
+        sendKeysToElement(searchBoxInputField, clothingItem);
+        return clothingItem;
+    }
+
+    public void clickSearchSubmitButton() {
+        safeClickOnElement(searchSubmitButton);
     }
 
 }
--- src/test/java/test_app/functional/ItemSearch.java
+++ src/test/java/test_app/functional/ItemSearch.java
@@ -1,7 +1,35 @@
 package test_app.functional;
 
+import app.pom.Homepage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 import test_base.TestBasePage;
 
 public class ItemSearch extends TestBasePage {
+
+    @Test
+    public void testSearchValidClothingItem() {
+        Homepage homepage = new Homepage();
+
+        String searchTerm = homepage.inputSearchTerm("dresses");
+        homepage.clickSearchSubmitButton();
+
+        String actualSearchTerm = homepage.actualSearchTerm.getText().replace("\"", "");
+        System.out.println(actualSearchTerm);
+
+        Assert.assertTrue(searchTerm.equalsIgnoreCase(actualSearchTerm));
+
+    }
+
+    @Test
+    public void testSearchInvalidClothingItem() {
+        Homepage homepage = new Homepage();
+
+        String searchTerm = homepage.inputSearchTerm("jackets");
+        homepage.clickSearchSubmitButton();
+
+        Assert.assertTrue(isElementVisible(homepage.noResultsFoundAlertWarning));
+
+    }
 
 }
